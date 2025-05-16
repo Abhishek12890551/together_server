5e9c874b1b5a41b26fd8a97e342eb796aa8dcb5c
Abhishek Kumar <mintu12890551@gmail.com>
2025-05-16 10:29:04 +0530

    fixed delete events

--- routes/eventRoute.js
+++ routes/eventRoute.js
@@ -12,6 +12,10 @@ const router = express.Router();
 
 router.route("/").get(protect, getEvents).post(protect, createEvent);
 
-router.route("/:id").get(getEventById).put(updateEvent).delete(deleteEvent);
+router
+  .route("/:id")
+  .get(protect, getEventById)
+  .put(protect, updateEvent)
+  .delete(protect, deleteEvent);
 
 export default router;
